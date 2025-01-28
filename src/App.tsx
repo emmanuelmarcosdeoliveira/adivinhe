b2b4312b03bb1cb5a210fa00a7f573e4ff5840e1
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2025-01-28 17:50:36 -0300

    style: developed the Tip component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import styles from "./app.module.css";
 import Header from "./components/Header";
+import Tip from "./components/Tip";
 
 function App() {
   function handleRestartGame() {
     alert("Jogo sendo reiniciado");
   }
-
   return (
     <section className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das linguagem de programação dinâmica mais utilizadas" />
       </main>
     </section>
   );
 }
-
 export default App;
