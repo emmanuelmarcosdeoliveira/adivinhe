20a9a9fcf5627fdbbdd190e07c41a5e041b04fd0
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2025-02-03 19:02:07 -0300

    feat: developed the functionality of inserting typed letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,78 @@
+import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Letter from "./components/Letter";
-import LettersUser from "./components/LettersUsed";
+import LettersUsed, { LetterUsedProps } from "./components/LettersUsed";
 import Tip from "./components/Tip";
+import { Challenge, WORDS } from "./utils/words";
 
 function App() {
+  const [letterUsed, setLetterUsed] = useState<LetterUsedProps[]>([]);
+  const [challenge, setChalenge] = useState<Challenge | null>(null);
+  const [attempts, setAttempts] = useState(0);
+  const [letter, setLetter] = useState("");
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randonWord = WORDS[index];
+    setChalenge(randonWord);
+    setAttempts(0);
+    setLetter("");
+  }
+
   function handleRestartGame() {
     alert("Jogo sendo reiniciado");
   }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+    if (!letter.trim()) {
+      alert("Digite uma  letra ");
+    }
+    const value = letter.toUpperCase();
+    const exist = letterUsed.find((used) => used.value.toUpperCase() === value);
+    if (exist) {
+      return alert("Voce já utilizou está letra" + value);
+    }
+    setLetterUsed((prevState) => [...prevState, { value, correct: false }]);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) {
+    return null;
+  }
+
   return (
     <section className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagem de programação dinâmica mais utilizadas" />
         <div className={styles.world}>
-          <Letter value="r" />
-          <Letter value="e" />
-          <Letter value="a" />
-          <Letter value="c" />
-          <Letter value="t" />
+          {challenge.word.split("").map(() => (
+            <Letter value="" />
+          ))}
         </div>
         <h4>Palpite</h4>
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button onClick={handleConfirm} title="Confirmar" />
         </div>
-        <LettersUser />
+        <LettersUsed data={letterUsed} />
       </main>
     </section>
   );
